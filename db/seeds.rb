a34cd6aca835f032a6b1aa81bf1ae5bf2ea0d613
celestialheathen <jingchaoyu@hotmail.com>
2021-02-14 11:39:40 -0500

    commit tree 803000475dfb444b110c21b4c6c2b1b680c1a7f9

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+post1 = Post.create(title: "Aya Neo to be launched after Chinese New Year", description: "Indigogo to be ready soon")
+post2 = Post.create(title: "Biden won presidency", description: "Republicans to protest results")
+post3 = Post.create(title: "Trump to be impeached", description: "Watch live coverage here")
+post4 = Post.create(title: "Project week incoming", description: "To be due next Friday")
+
